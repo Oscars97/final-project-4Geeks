36f030fcabf8d17524dc47114cfcbd17008a70f0
Óscar Sánchez <oscarsanchezmeza97@gmail.com>
2021-03-09 17:09:33 +0000

    the database is updated

--- src/api/models.py
+++ src/api/models.py
@@ -3,10 +3,13 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 class User(db.Model):
+    __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    profile = db.relationship('Profile', backref='parent',lazy=True)
+    posts = db.relationship('Post', backref='parent',lazy=True)
 
     def __repr__(self):
         return '<User %r>' % self.username
@@ -15,5 +18,53 @@ class User(db.Model):
         return {
             "id": self.id,
             "email": self.email,
+            "username": self.mail
+            # do not serialize the password, its a security breach
+        }
+
+
+class Profile(db.Model):
+    __tablename__ = 'profile'
+    id = db.Column(db.Integer, primary_key=True)
+    about_me = db.Column(db.String(300), unique=False, nullable=False)
+    name = db.Column(db.String(50), unique=False, nullable=False)
+    last_name = db.Column(db.String(50), unique=False, nullable=False)
+    twitter = db.Column(db.String(80), unique=False, nullable=True)
+    github = db.Column(db.String(80), unique=False, nullable=True)
+    linkedin = db.Column(db.String(80), unique=False, nullable=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+    
+    
+    def __repr__(self):
+        return '<Profile %r>' % self.name
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "last_name": self.last_name,
+            "about_me": self.about_me,
+            "twitter": self.twitter,
+            "github": self.github,
+            "linkedin": self.linkedin 
+            # do not serialize the password, its a security breach
+        }
+
+class Post(db.Model):
+    __tablename__ = 'post'
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(300), unique=False, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+    
+    
+    def __repr__(self):
+        return '<Post %r>' % self.id
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "content": self.content,
+            "user_id": self.user_id
+            
             # do not serialize the password, its a security breach
         }
