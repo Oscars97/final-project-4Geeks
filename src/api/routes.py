aa010229530f525f6806c7149d95ddf59c16277c
Angiegeek <avm03021993@gmail.com>
2021-03-11 18:28:34 +0000

    algo mas

--- src/api/routes.py
+++ src/api/routes.py
@@ -4,8 +4,11 @@ This module takes care of starting the API Server, Loading the DB and Adding the
 from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User, Profile, Post
 from api.utils import generate_sitemap, APIException
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity 
 
 api = Blueprint('api', __name__)
+jwt = JWTManager(api)
 
 
 @api.route('/hello', methods=['POST', 'GET'])
