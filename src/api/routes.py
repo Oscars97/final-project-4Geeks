2c82daf5e4d8e6e75a5b9ddc2f0ee941ab8d9c96
IsmaelArtavia <ismaelartavia@yahoo.es>
2021-03-11 18:28:57 +0000

    actualizacion

--- src/api/routes.py
+++ src/api/routes.py
@@ -4,9 +4,11 @@ This module takes care of starting the API Server, Loading the DB and Adding the
 from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User, Profile, Post
 from api.utils import generate_sitemap, APIException
+rom werkzeug.security import generate_password_hash, check_password_hash       ## Nos permite manejar tokens por authentication (usuarios)    
+from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity   #from models import Person
 
 api = Blueprint('api', __name__)
-
+#jwt = JWTManager(api)
 
 @api.route('/hello', methods=['POST', 'GET'])
 def handle_hello():
