0cb04457788e31024212775f5fe108e3b6f57eb4
Óscar Sánchez <oscarsanchezmeza97@gmail.com>
2021-03-11 17:21:51 +0000

    Adding 200 chars to password

--- src/api/models.py
+++ src/api/models.py
@@ -6,7 +6,7 @@ class User(db.Model):
     __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
+    password = db.Column(db.String(200), unique=False, nullable=False)
     username = db.Column(db.String(80), unique=True, nullable=False)
     profile = db.relationship('Profile', backref='parent',lazy=True)
     posts = db.relationship('Post', backref='parent',lazy=True)
