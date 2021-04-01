8ce02301b5f0a43ac9b6fe95055d31f2926a7bdb
Óscar Sánchez <oscarsanchezmeza97@gmail.com>
2021-04-01 16:53:03 +0000

    deploying to heroku

--- src/front/js/index.js
+++ src/front/js/index.js
@@ -8,6 +8,7 @@ import "bootstrap/dist/css/bootstrap.css";
 //include your index.scss file into the bundle
 import "../styles/index.scss";
 
+
 //import your own components
 import Layout from "./layout";
 
